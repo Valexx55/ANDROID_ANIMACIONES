9490df1d5c5f84c58687dcf17248a515bdc9e98c
formador <formador@aula8PovMacNfor.local>
2017-05-20 14:28:09 +0200

    v1 animas

---
+++ MyAnimationApp2/app/src/main/java/com/example/formador/myanimationapp2/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.formador.myanimationapp2;
+
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView tv = (TextView) findViewById(R.id.gol);
+
+        //ANIMATION VIEW "TRADITIONAL"
+        /*Animation a = AnimationUtils.loadAnimation(this, R.anim.animation_view);
+        tv.startAnimation(a);*/
+
+        //ANIMATION PROPERTY OBJECTANIMATOR
+        /*ValueAnimator va = (ValueAnimator) AnimatorInflater.loadAnimator(this, R.animator.animacion_property_object);
+        va.setTarget(tv);
+        va.start();*/
+
+        //ANIMATION PROPERTY VALUEANIMATOR
+        /*ValueAnimator va1 = (ValueAnimator)AnimatorInflater.loadAnimator(this, R.animator.animacion_property_animator);
+        va1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float animated_value = (float)animation.getAnimatedValue();
+                tv.setTranslationY(animated_value);
+
+            }
+        });
+
+        va1.start();*/
+
+        //ANIMATION PROPERTY SET
+        AnimatorSet animatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.animacion_property_set);
+        animatorSet.setTarget(tv);
+        animatorSet.start();
+    }
+}
